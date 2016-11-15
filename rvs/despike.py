41351c6e786efecaa8135623063fcaafafb49cd8
Meredith Rawls <mrawls@uw.edu>
2016-11-14 21:19:12 -0800

    add option for despiking spectra in text format, not just FITS

--- rvs/despike.py
+++ rvs/despike.py
@@ -13,29 +13,41 @@ speclist = []
 #filelist = '/Users/revhalzoo/SDSS/B5285607/B5285607infiles.txt'
 #filelist = '/Users/revhalzoo/SDSS/C6449358/C6449358infiles.txt'
 #filelist = '/Users/revhalzoo/SDSS/DCA/DChoAinfiles.txt'
-filelist = '/Users/revhalzoo/SDSS/A4851217/A4851217phx.txt'
+#filelist = '/Users/revhalzoo/SDSS/A4851217/A4851217phx.txt'
+filelist = 'data/4851217/A4851217infiles.txt'
 f1 = open(filelist)
 infilelist = [] # for use later to make outfilelist
 
 # option to just turn the fits file into a text file without any despiking
-doDespike = False
+doDespike = True
+isFits = False
 
-for line in f1:
-    infile = line.rstrip()
-    infilelist.append(infile)
-    print (infile)
-    hdu = fits.open(infile)
-    # APOGEE! the data is in a funny place and backwards
-    wave = hdu[4].data
-    wave = wave.flatten()
-    wave = wave[::-1]
-    spec = hdu[1].data
-    spec = spec.flatten()
-    spec = spec[::-1]
-    spec = spec / np.median(spec) # put the continuum roughly near 1
-    wavelist.append(wave)
-    speclist.append(spec)
-f1.close()
+if isFits == True: #it's a FITS file
+    for line in f1:
+        infile = line.rstrip()
+        infilelist.append(infile)
+        print (infile)
+        hdu = fits.open(infile)
+        # APOGEE! the data is in a funny place and backwards
+        wave = hdu[4].data
+        wave = wave.flatten()
+        wave = wave[::-1]
+        spec = hdu[1].data
+        spec = spec.flatten()
+        spec = spec[::-1]
+        spec = spec / np.median(spec) # put the continuum roughly near 1
+        wavelist.append(wave)
+        speclist.append(spec)
+    f1.close()
+
+else: # it's a text file
+    for line in f1:
+        infile = line.rstrip()
+        infilelist.append(infile)
+        wave, spec = np.loadtxt(infile, usecols=(0,1), unpack=True)
+        wavelist.append(wave)
+        speclist.append(spec)
+    f1.close()
 
 # do something to 'spec' to cut out spikes, call it newspec
 newwavelist = []
@@ -61,8 +73,9 @@ for wave, spec in zip(wavelist, speclist):
 # each containing one element of wavelist and one element of newspeclist
 for file, wave, newspec in zip(infilelist, wavelist, newspeclist):
     # create outfile based on infile name
-    outfile = file[0:-5]+'_textified.txt'
-    #print(outfile)
+    outfile = file[0:-4]+'_despiked.txt'
+    print(outfile)
     f = open(outfile, 'w')
     for wentry, sentry in zip(wave, newspec):
         print(wentry, sentry, file=f)
+
