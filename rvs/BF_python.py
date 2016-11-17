28dee48486663240172d8d381e9b84d62acdc98b
Joni Marie Clark Cunningham <jonimarieclarkcunningham@gmail.com>
2016-11-16 19:01:21 -0700

    Slight changes to input files, added plots

--- rvs/BF_python.py
+++ rvs/BF_python.py
@@ -74,16 +74,16 @@ both in days, and the constant RV and BCV of whatever template you are using.
 #outfile =   'data/4851217/A4851217Outfile.txt'
 
 #B5285607
-infiles =   'data/5285607/5285607infiles.txt'
-bjdinfile = 'data/5285607/5285607bjdinfile.txt'
-gausspars = 'data/5285607/5285607gausspars.txt'
-outfile =   'data/5285607/5285607Outfile.txt'
+#infiles =   'data/5285607/5285607infiles.txt'
+#bjdinfile = 'data/5285607/5285607bjdinfile.txt'
+#gausspars = 'data/5285607/5285607gausspars.txt'
+#outfile =   'data/5285607/5285607Outfile.txt'
 
 #C6449358
-#infiles =   '/Users/revhalzoo/SDSS/C6449358/C6449358infiles.txt'
-#bjdinfile = '/Users/revhalzoo/SDSS/C6449358/C6449358bjdinfile.txt'
-#gausspars = '/Users/revhalzoo/SDSS/C6449358/C6449358gausspars.txt'
-#outfile =   '/Users/revhalzoo/SDSS/C6449358/C6449358Outfile.txt'
+infiles =   'data/6449358/C6449358infiles.txt'
+bjdinfile = 'data/6449358/C6449358bjdinfile.txt'
+gausspars = 'data/6449358/C6449358gausspars.txt'
+outfile =   'data/6449358/C6449358Outfile.txt'
 
 # STUFF YOU NEED TO DEFINE CORRECTLY !!!
 isAPOGEE = True        # toggle to use near-IR stuff, or not
@@ -95,8 +95,8 @@ widlimits = [0,5, 0,5]   # limits for gaussian width (km/s) [min1,max1,min2,max2
 
 # ORBITAL PERIOD AND ZEROPOINT !!!
 #period = 2.47028; BJD0 = 2455813.69734 #(A4851217)
-period = 3.8994011; BJD0 = 2455813.69734 #(B5285607)
-#period = 5.7767904; BJD0 = 2456760.90580 #(C6449358)
+#period = 3.8994011; BJD0 = 2455813.69734 #(B5285607)
+period = 5.7767904; BJD0 = 2456760.90580 #(C6449358)
 
 # RADIAL VELOCITY AND BCV INFO FOR TEMPLATE (km/s; set both to 0 if using a model !!!)
 rvstd = 0; bcvstd = 0 # model template
