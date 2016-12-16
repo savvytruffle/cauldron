4aa1135e3b0e0e91bbbc271476c444451ea27fab
Joni Marie Clark Cunningham <jonimarieclarkcunningham@gmail.com>
2016-12-16 13:10:05 -0700

    Putting txt files in chron order 4851217

--- rvs/phoenix2txt.py
+++ rvs/phoenix2txt.py
@@ -53,10 +53,11 @@ wavedir = 'data/PHOENIX/'
 specdir = 'data/PHOENIX/Z-0.0/'
 # spectrum filename:
 #specfile = 'lte05500-4.50-0.0.PHOENIX-ACES-AGSS-COND-2011-HiRes.fits' #example
+specfile = 'lte06900-4.50-0.0.PHOENIX-ACES-AGSS-COND-2011-HiRes.fits' #4851217
 #specfile = 'lte06400-5.00-0.0.PHOENIX-ACES-AGSS-COND-2011-HiRes.fits' #5285607
 #specfile = 'lte06500-5.00-0.0.PHOENIX-ACES-AGSS-COND-2011-HiRes.fits' #6449358
 #specfile = 'lte05700-4.50-0.0.PHOENIX-ACES-AGSS-COND-2011-HiRes.fits' #6781535
-specfile = 'lte04200-5.50-0.0.PHOENIX-ACES-AGSS-COND-2011-HiRes.fits' #5284133
+#specfile = 'lte04200-5.50-0.0.PHOENIX-ACES-AGSS-COND-2011-HiRes.fits' #5284133
 
 #######################
 ## edit values above ##
