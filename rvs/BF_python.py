bcd772c656df6816e96e7b32b5deb114bb9b68a5
Joni Marie Clark Cunningham <jonimarieclarkcunningham@gmail.com>
2016-12-17 22:26:43 -0700

    6778289 Crazy Spikey BFURV.

--- rvs/BF_python.py
+++ rvs/BF_python.py
@@ -81,16 +81,22 @@ both in days, and the constant RV and BCV of whatever template you are using.
 #bfoutfile = 'data/5285607/5285607BFdata.txt'
 
 #6449358
-infiles =   'data/6449358/6449358infiles.txt'
-bjdinfile = 'data/6449358/6449358bjdinfile.txt'
-gausspars = 'data/6449358/6449358gausspars.txt'
-outfile =   'data/6449358/6449358Outfile.txt'
+#infiles =   'data/6449358/6449358infiles.txt'
+#bjdinfile = 'data/6449358/6449358bjdinfile.txt'
+#gausspars = 'data/6449358/6449358gausspars.txt'
+#outfile =   'data/6449358/6449358Outfile.txt'
 
 #5284133
 #infiles =   'data/5284133/5284133infiles.txt'
 #bjdinfile = 'data/5284133/5284133bjdinfile.txt'
 #gausspars = 'data/5284133/5284133gausspars.txt'
 #outfile =   'data/5284133/5284133Outfile.txt'
+
+#6778289
+infiles =   'data/6778289/6778289infiles.txt'
+bjdinfile = 'data/6778289/6778289bjdinfile.txt'
+gausspars = 'data/6778289/6778289gausspars.txt'
+outfile =   'data/6778289/6778289Outfile.txt'
 
 # STUFF YOU NEED TO DEFINE CORRECTLY !!!
 isAPOGEE = True        # toggle to use near-IR stuff, or not
@@ -104,9 +110,9 @@ threshold = 10             # margin for gaussian position (raw RV in km/s)
 # ORBITAL PERIOD AND ZEROPOINT !!!
 #period = 2.47028; BJD0 = 2455813.69734 # 4851217
 #period = 3.8994011; BJD0 = 2454959.576010 # 5285607
-period = 5.7767904; BJD0 = 2456760.90580 # 6449358
+#period = 5.7767904; BJD0 = 2456760.90580 # 6449358
 #period = 8.7845759; BJD0 = 245800.46231 #5284133
-#period = 30.13015; BJD0 = 2456557.73097 #6778289
+period = 30.13015; BJD0 = 2456557.73097 #6778289
 #period = 9.1220856; BJD0 = 2456557.733 #6781535
 
 # RADIAL VELOCITY AND BCV INFO FOR TEMPLATE (km/s; set both to 0 if using a model !!!)
