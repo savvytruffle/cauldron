78c6106e20271d7b766d231cf0b8fe90ca64fd70
Meredith Rawls <mrawls@uw.edu>
2016-12-28 18:08:58 -0800

    adjust BF_python plot colors and axes

--- rvs/BF_python.py
+++ rvs/BF_python.py
@@ -189,9 +189,9 @@ for i in range (0, nspec):
     newspeclist.append(newspec)
     if SpecPlot == True:
         if i == 0: # plot template in red
-            plt.plot(w1, newspec+yoffset, label=datetimelist[i].iso[0:10], color='r', marker='.')
+            plt.plot(w1, newspec+yoffset, label=datetimelist[i].iso[0:10], color=colors[6], marker='.')
         else: # plot the rest in blue
-            plt.plot(w1, newspec+yoffset, label=datetimelist[i].iso[0:10], color='b', marker='.')
+            plt.plot(w1, newspec+yoffset, label=datetimelist[i].iso[0:10], color=colors[0], marker='.')
     yoffset = yoffset + 1
 if SpecPlot == True:
     ##plt.legend()
@@ -245,8 +245,8 @@ plt.xlabel('Radial Velocity (km s$^{-1}$)')
 plt.ylabel('Broadening Function (arbitrary amplitude)')
 yoffset = 0.0
 for i in range(1, nspec):
-    plt.plot(bf_ind, bfnormlist[i]+yoffset, color='b', marker='.')
-    plt.axhline(y=yoffset, color='0.75', ls=':')
+    plt.plot(bf_ind, bfnormlist[i]+yoffset, color=colors[0], marker='.')
+    plt.axhline(y=yoffset, color=colors[15], ls=':')
     yoffset = yoffset + 1.0
 plt.show()
 
@@ -314,7 +314,7 @@ def gaussian(x, amp, mu, sig): # i.e., (xarray, amp, rv, width)
 # PLOT THE FINAL SMOOTHED BFS + GAUSSIAN FITS IN INDIVIDUAL PANELS
 # manually adjust this multi-panel plot based on how many spectra you have
 #plt.figure(4)
-windowcols = 3                                # how many columns the plot should have
+windowcols = 3 # 4                             # how many columns the plot should have
 #windowrows = 6                                # manually set number of plot rows here, or automatically below
 windowrows = int([np.rint((nspec-1)/windowcols) if (np.float(nspec-1)/windowcols)%windowcols == 0 else np.rint((nspec-1)/windowcols)+1][0])
 xmin = rvneg
@@ -325,9 +325,11 @@ fig.text(0.5, 0.04, 'Uncorrected Radial Velocity (km s$^{-1}$)', ha='center', va
 fig.text(0.07, 0.6, 'Broadening Function', ha='center', va='center', size=26, rotation='vertical')
 #########0.07, 0.5
 for i in range (1,nspec):
-    ax = fig.add_subplot(windowrows, windowcols,i) # out of range if windowcols x windowrows < nspec
+    ax = fig.add_subplot(windowrows, windowcols, i) # out of range if windowcols x windowrows < nspec
     ax.yaxis.set_major_locator(MultipleLocator(0.2))
-    if i!=1 and i!=5 and i!=9 and i!=13 and i!=17 and i!=21 and i!=25:
+    if windowcols == 4 and (i!=1 and i!=5 and i!=9 and i!=13 and i!=17 and i!=21 and i!=25):
+        ax.set_yticklabels(())
+    if windowcols == 3 and (i!=1 and i!=4 and i!=7 and i!=10 and i!=13 and i!=16 and i!=19 and i!=22 and i!=25):
         ax.set_yticklabels(())
     #if i!=20 and i!=21 and i!=22 and i!=23 and i!=24 and i!=25:
     if i < nspec-windowrows:
@@ -336,17 +338,17 @@ for i in range (1,nspec):
     plt.subplots_adjust(wspace=0, hspace=0)
     plt.axis([xmin, xmax, ymin, ymax])
     plt.tick_params(axis='both', which='major', labelsize=14)
-    plt.text(xmax - 0.25*(np.abs(xmax-xmin)), 0.8*ymax, '%.3f $\phi$' % (phase[i]), size=12)
-    plt.text(xmax - 0.35*(np.abs(xmax-xmin)), 0.6*ymax, '%s' % (datetimelist[i].iso[0:10]), size=12)
-    #plt.plot(bf_ind, bfsmoothlist[i], color='k', lw=1.5, ls='-', label='Smoothed BF')
-    plt.plot(bf_ind, bfnormlist[i], color='k', lw=1.5, ls='-', label='Normalized Smoothed BF')
-    plt.plot(bf_ind, bffitlist[i][1], color='b', lw=2, ls='--', label='Two Gaussian fit')
+    plt.text(xmax - 0.25*(np.abs(xmax-xmin)), 0.85*ymax, '%.3f $\phi$' % (phase[i]), size=12)
+    plt.text(xmax - 0.35*(np.abs(xmax-xmin)), 0.70*ymax, '%s' % (datetimelist[i].iso[0:10]), size=12)
+    #plt.plot(bf_ind, bfsmoothlist[i], color=colors[14], lw=1.5, ls='-', label='Smoothed BF')
+    plt.plot(bf_ind, bfnormlist[i], color=colors[14], lw=1.5, ls='-', label='Normalized Smoothed BF')
+    plt.plot(bf_ind, bffitlist[i][1], color=colors[0], lw=3, ls='-', label='Two Gaussian fit')
     gauss1 = gaussian(bf_ind, bffitlist[i][0][0], bffitlist[i][0][1], bffitlist[i][0][2])
     gauss2 = gaussian(bf_ind, bffitlist[i][0][3], bffitlist[i][0][4], bffitlist[i][0][5])
-    plt.plot(bf_ind, gauss1, color='#e34a33', lw=2, ls='--')#, label='Gaussian fit 1')
-    plt.plot(bf_ind, gauss2, color='#fdbb84', lw=2, ls='--')#, label='Gaussian fit 2')
+    plt.plot(bf_ind, gauss1, color=colors[6], lw=3, ls='--')#, label='Gaussian fit 1')
+    plt.plot(bf_ind, gauss2, color=colors[2], lw=3, ls='--')#, label='Gaussian fit 2')
     # OPTION TO PLOT VERTICAL LINE AT ZERO
-    plt.axvline(x=0, color='0.75')    
+    plt.axvline(x=0, color=colors[15])    
     # print legend
     if i==nspec-1: ax.legend(bbox_to_anchor=(2.6,0.7), loc=1, borderaxespad=0., 
                         frameon=False, handlelength=3, prop={'size':20})
