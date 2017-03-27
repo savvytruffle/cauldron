e8171c028ceebe5af658e74f53b686f36617174a
Meredith Rawls <mrawls@uw.edu>
2017-03-26 21:50:29 -0700

    Add multi-panel plotting capabilities with loops over CCF and BF data. WARNING: there is presently no check to make sure the two things plotted on top of each other both correspond to the same visit!

--- rvs/BF_CCF_plotter.py
+++ rvs/BF_CCF_plotter.py
@@ -23,7 +23,7 @@ CCF_w0 = hdu9['CCFW0'][0]  # starting wavelength in Angstroms ??
 CCF_delta = hdu9['CCFDW'][0]  # Delta (log_10 lambda)
 # log lambda spacing per lag step (6.d-6 corresponds to 4.145 km/s)
 
-# Get the systemic RV for each visit
+# Get the systemic RV for each visit according to APOGEE
 ## THIS IS NON-TRIVIAL. APOGEE really likes to remove all RV information.
 ## There are VREL1 - VRELNVISIT in HDU0, but they're just BC + VREL = VHELIO.
 ## This is not helpful.
@@ -37,12 +37,28 @@ systemics = hdu9['SYNTHVREL'][0]
 CCF_rvaxis = []
 for rv in systemics:
     CCF_rvaxis.append(CCFxaxis * 4.145 + rv)
-
 # for now, let's just use:
 CCF_rvaxis = CCFxaxis * 4.145
 
+# Set up the main figure
+fig = plt.figure(1, figsize=(12,8))
+windowrows = 7
+windowcols = 4
+fig.text(0.5, 0.04, 'Arbitrary RV (km s$^{-1}$)', ha='center', va='center', size='small')
+fig.text(0.07, 0.6, 'CCF or BF amplitude', ha='center', va='center', size='small', rotation='vertical')
+
+# Loop over and plot CCF data
+for idx, CCFdata in enumerate(CCFvalues):
+    if idx >= 2: # only plot visits, not 0th and 1st columns (combined spectra)
+        ax = fig.add_subplot(windowrows, windowcols, idx-1)
+        plt.axis([-150, 150, -0.1, 0.5])
+        ax.set_xticklabels(()) # add some of these back in later for certain idx
+        ax.set_yticklabels(()) # add some of these back in later for certain idx
+        plt.plot(CCF_rvaxis, CCFvalues[idx] - 0.2) 
+        plt.subplots_adjust(wspace=0, hspace=0)   
+
 # Read in relevant BF info for the same target
-bffile = '6864859BFOut.txt'
+bffile = '6864859BFOut.txt' # WARNING: we omitted some visits from this BF run!
 bfinfile = dir + bffile
 
 # Read in relevant BF info from the BF infile
@@ -51,20 +67,31 @@ BFrvaxis = bfdata[0]
 BFvalues = bfdata[1]
 BFerrors = bfdata[2]
 
-# Starter loop, NOT FINISHED YET. Need to save the index of where each visit starts/ends.
+# Save the indices that separate each BF's visit in the input file
 visitidx = 0
+BFindices = []
 for idx, (rv, value, error) in enumerate(zip(BFrvaxis, BFvalues, BFerrors)):
     if np.abs(BFrvaxis[idx-1] - rv) > 100:
         visitidx = visitidx + 1
-        #print(visitidx, idx)
-        # save idx at this point; e.g., it should be 400 on the first time through
+        BFindices.append(idx)
+print(BFindices)
 
-# Meanwhile, for just one visit, we can hardwire [0:400] as the start:end.
-# And we can also adjust the RV zeropoints and BF amplitudes arbitrarily.
-plt.plot(BFrvaxis[0:400] - 107, 10*BFvalues[0:400])
-plt.plot(CCF_rvaxis, CCFvalues[2] - 0.2)
-plt.axis([-150, 150, -0.1, 0.5])
-plt.xlabel('Arbitrary radial velocity')
-plt.ylabel('CCF or BF amplitude')
+# Loop over and plot BF data
+## You will get several WARNING statements printed if there are fewer BFs than CCFs,
+## (and you will also notice that not every plot window has a BF on top of the CCF)
+xoffset = 100 # arbitrary systemic RV offset to make CCF and BF line up
+yamp = 8 # arbitrary factor to stretch BF values to make CCF and BF line up
+for idx in range(0, len(CCFvalues)-2):
+    ax = fig.add_subplot(windowrows, windowcols, idx+1)
+    plt.axis([-150, 150, -0.1, 0.5])
+    ax.set_xticklabels(())
+    ax.set_yticklabels(())
+    try:
+        plt.plot(BFrvaxis[BFindices[idx]:BFindices[idx+1]] - xoffset, 
+                 yamp*BFvalues[BFindices[idx]:BFindices[idx+1]])
+    except:
+        print('WARNING: BF index {0} is out of range, skipping'.format(idx))
+        continue
+
 plt.show()
 
