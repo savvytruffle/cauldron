1afe9d132fdc17645cc021b015307ddbde9f8435
Joni Marie Clark Cunningham <jonimarieclarkcunningham@gmail.com>
2017-03-28 14:29:35 -0700

    6449358

--- rvs/BF_python.py
+++ rvs/BF_python.py
@@ -92,9 +92,9 @@ both in days, and the constant RV and BCV of whatever template you are using.
 #outfile = 'data/4285087/4285087outfile.txt'; bfoutfile = 'data/4285087/4285087BFdata.txt'
 
 #6449358
-#infiles =   'data/6449358/6449358infiles.txt'; bjdinfile = 'data/6449358/6449358bjdinfile.txt'
-#gausspars = 'data/6449358/6449358gausspars.txt'
-#outfile =   'data/6449358/6449358Outfile1.txt'; bfoutfile = 'data/6449358/6449358BFOut.txt'
+infiles =   'data/6449358/6449358infiles.txt'; bjdinfile = 'data/6449358/6449358bjdinfile.txt'
+gausspars = 'data/6449358/6449358gausspars.txt'
+outfile =   'data/6449358/6449358Outfile1.txt'; bfoutfile = 'data/6449358/6449358BFOut.txt'
 
 #5284133
 #infiles =   'data/5284133/5284133infiles.txt'; bjdinfile = 'data/5284133/5284133bjdinfile.txt'
@@ -102,9 +102,9 @@ both in days, and the constant RV and BCV of whatever template you are using.
 #outfile =   'data/5284133/5284133Outfile.txt'; bfoutfile = 'data/5284133/5284133BFOut.txt'
 
 #6778289
-infiles =   'data/6778289/6778289infiles.txt'; bjdinfile = 'data/6778289/6778289bjdinfile.txt'
-gausspars = 'data/6778289/6778289gausspars.txt'
-outfile =   'data/6778289/6778289Outfile.txt'; bfoutfile = 'data/6778289/6778289BFOut.txt'
+#infiles =   'data/6778289/6778289infiles.txt'; bjdinfile = 'data/6778289/6778289bjdinfile.txt'
+#gausspars = 'data/6778289/6778289gausspars.txt'
+#outfile =   'data/6778289/6778289Outfile.txt'; bfoutfile = 'data/6778289/6778289BFOut.txt'
 
 #6781535
 #infiles =   'data/6781535/6781535infiles.txt'; bjdinfile = 'data/6781535/6781535bjdinfile.txt'
@@ -119,9 +119,9 @@ outfile =   'data/6778289/6778289Outfile.txt'; bfoutfile = 'data/6778289/6778289
 # ORBITAL PERIOD AND ZEROPOINT !!!
 #period = 2.47028; BJD0 = 2455813.69734 # 4851217
 #period = 3.8994011; BJD0 = 2454959.576010 # 5285607
-#period = 5.7767904; BJD0 = 2456760.90580 # 6449358
+period = 5.7767904; BJD0 = 2456760.90580 # 6449358
 #period = 8.7845759; BJD0 = 245800.46231 #5284133
-period = 30.13015; BJD0 = 2456557.73097 #6778289
+#period = 30.13015; BJD0 = 2456557.73097 #6778289
 #period = 9.1220856; BJD0 = 2456557.733 #6781535
 #period = 40.8778427; BJD0 = 2454955.556300 #6864859
 #period = 61.4228063; BJD0 = 2455813.69734 #4075064
@@ -155,8 +155,8 @@ w00 = 15170; n = 22000; stepV = 1.5 # all of APOGEE, still pretty high res
 # CUSTOMIZED BF WIDTH AND PLOT LIMITS
 #widlimits = [0,15, 0,15]; rvneg = -100; rvpos = 100; ymin = -0.15; ymax = 1.19 # good starting default
 #widlimits = [0,15, 0,15]; rvneg = -70; rvpos = 270; ymin = -0.15; ymax = 1.19 # 5285607
-#widlimits = [0,5, 0,5]; rvneg = 0; rvpos = 200; ymin = -0.15; ymax = 1.1 #6449358
-widlimits = [0,5, 0,5]; rvneg = 150; rvpos = 300; ymin = -0.15; ymax = 1.1 #6778289
+widlimits = [0,5, 0,5]; rvneg = 0; rvpos = 200; ymin = -0.15; ymax = 1.1 #6449358
+#widlimits = [0,5, 0,5]; rvneg = 0; rvpos = 200; ymin = -0.15; ymax = 1.1 #6778289
 #widlimits = [0,9, 0,9]; rvneg = 30; rvpos = 170; ymin = -0.15; ymax = 1.19 # 6864859
 
 colors = bff.user_rc()
