947c3bb7cbe57974b2114ca4aecf9f32d8adb41f
Meredith Rawls <mrawls@uw.edu>
2017-05-23 19:44:48 -0700

    Refactored apVisit2input to be modular and much friendlier!

--- rvs/apVisitproc/apvisitproc/apVisit2input.py
+++ rvs/apVisitproc/apvisitproc/apVisit2input.py
@@ -1,79 +1,126 @@
 from __future__ import print_function
 import numpy as np
 import apogee.tools.read as apread
-from apogee.tools import apStarWavegrid
-import apogee.spec.plot as splot
 from apogee.spec import continuum
 import matplotlib.pyplot as plt
-import apogee as apg
-from apogee.modelspec import ferre
 import os
 from astropy.io import fits
-import csv
+import argparse
 '''
 This program uses jobovy/apogee to make text files for APOGEE visit spectra and a model spectrum.
 The model spectrum should have stellar parameters similar to the target star for use with BF_python.
 All the final spectra are continuum normalized.
+
+USAGE:
+$ python apVisit2input.py -d datadir -k KIC
+This assumes that the apVisit metadata file is datadir/KIC/KICVisitlist.txt
+e.g.,
+$ python apVisit2input.py -d data -k 1234567
 '''
-### define useful variables upfront here so they're easy to change in one place. ###
-KIC = 5285607
-#ApogeeID = '2M19432016+3957081'
 
-locIDs, mjds, fiberIDs = np.loadtxt('data/' + str(KIC) +'/' + str(KIC) + 'Visitlist.txt', 
-    usecols=(1, 2, 3), unpack=True, delimiter=',')
 
-infilelist = []; HJDlist = []; BCVlist = []
+def main():
+    '''
+    Read datadir and KIC from command line
+    Load data for all the visits for the target specified
+    Loop over all the visits, normalize the spectra, and save to file
+    Print the names of the new files with their HJDs and BCVs
+    '''
+    # parse command line arguments with argparse
+    parser = argparse.ArgumentParser(description='Run with python apVisit2input.py -d datadir -k KIC')
+    parser.add_argument('-d', dest='datadir', required=True,
+                        help='directory containing KIC subdirectories')
+    parser.add_argument('-k', dest='KIC', required=True,
+                        help='KIC of target')
+    args = parser.parse_args()
+    datadir = args.datadir
+    KIC = args.KIC
 
-for locID, mjd, fiberID in zip(locIDs, mjds, fiberIDs):
-    # the three arguments are location ID, MJD, and fiber ID (currently floats)
-    # they need to be strings of length 4, 5, and 3, respectively
-    locID = '{:04d}'.format(int(locID))
-    mjd = '{:05d}'.format(int(mjd))
-    fiberID = '{:03d}'.format(int(fiberID))
-    #print('locID, mjd, fiberID: ', locID, mjd, fiberID) # test to make sure the values are correct
-    
-### Trying to fix the issue with spectra not being found on the server ###
-    locID = int(locID)
+    locIDs, mjds, fiberIDs = load_allvisitinfo(datadir, KIC)
+    print('New spectrum file (despiked), HJD, and BCV:')
 
-### Make a unique outfile for each visit spectrum ###
-    specfileout = 'data/'+str(KIC)+'/apVisitnorm-'+str(locID)+'-'+str(mjd)+'-'+str(fiberID)+'.txt'
-    infilelist.append(specfileout)   
-    #test = apread.apVisit(int(allVisit['PLATE'][i].strip()), allVisit['MJD'][i], allVisit['FIBERID'][i], ext=1, header=False)
-    
-    fluxes = apread.apVisit(locID, mjd, fiberID, ext=1, header=False)
-    fluxerrs = apread.apVisit(locID, mjd, fiberID, ext=2, header=False)
-    waves = apread.apVisit(locID, mjd, fiberID, ext=4, header=False)
-    #header = apread.apVisit(locID, mjd, fiberID, ext=1, header=True) # only gives 1st extension header data
-    
-    # Manually access the file you just downloaded and read the header from it (ugh, sorry)
-    #SDSSland = os.environ.get('SDSS_LOCAL_SAS_MIRROR')
-    #fitsfilepath = (SDSSland + '/dr12/apogee/spectro/redux/r5/apo25m/' + str(locID) + '/' + str(mjd) + 
-    #               '/apVisit-r5-'+ str(locID) + '-' + str(mjd) + '-' + str(fiberID) + '.fits'
-    
-### Trying to fix the issue with spectra not being found on the server ###
-    fitsfilepath = 'data/'+str(KIC)+'/apVisit-r5-'+ str(locID) + '-' + str(mjd) + '-' + str(fiberID) + '.fits'
+    # loop over all visit spectra
+    for locID, mjd, fiberID in zip(locIDs, mjds, fiberIDs):
+        fitsfilepath, specfileout, wave, flux, fluxerr = load_apVisit(datadir, KIC, locID, mjd, fiberID)
+        specnorm, specnormerr = normalize_spec(wave, flux, fluxerr, plot=True)
+        with open(specfileout, 'w') as f:
+            for wavepoint, specpoint, specnormpoint in zip(wave, specnorm, specnormerr):
+                print(wavepoint, specpoint, specnormpoint, file=f)
+        HJD, BCV = make_BFinfile(fitsfilepath)
+        print(specfileout, HJD, BCV)
+    return
+
+
+def load_allvisitinfo(datadir, KIC):
+    '''
+    runs on a list of visit spectra
+    returns lists of fits files on disk, locIDs, mjds, and fiberIDs for each visit
+    '''
+    visitlist = os.path.join(datadir, KIC, KIC + 'Visitlist.txt')
+    if not os.path.exists(visitlist):
+        raise argparse.ArgumentTypeError("{0} does not exist".format(visitlist))
+    locIDs, mjds, fiberIDs = np.loadtxt(visitlist, usecols=(1, 2, 3),
+                                        unpack=True, delimiter=',')
+    return locIDs, mjds, fiberIDs
+
+
+def load_apVisit(datadir, KIC, locID, mjd, fiberID):
+    '''
+    runs on a single visit spectrum
+    returns the new outfile name and the raw data from the visit spectrum
+    '''
+    locID = str(int('{:04d}'.format(int(locID))))
+    mjd = str('{:05d}'.format(int(mjd)))
+    fiberID = str('{:03d}'.format(int(fiberID)))
+    specfileout = os.path.join(datadir, KIC, 'apVisitnorm-'+locID+'-'+mjd+'-'+fiberID+'.txt')
+    wave = apread.apVisit(int(locID), mjd, fiberID, ext=4, header=False)
+    flux = apread.apVisit(int(locID), mjd, fiberID, ext=1, header=False)
+    fluxerr = apread.apVisit(int(locID), mjd, fiberID, ext=2, header=False)
+    SDSS_PATH = os.environ.get('SDSS_LOCAL_SAS_MIRROR')
+    SDSS_VERSION = os.environ.get('RESULTS_VERS')
+    if SDSS_PATH is None:
+        raise RuntimeError('You haven\'t defined the environment variable SDSS_LOCAL_SAS_MIRROR')
+    if SDSS_VERSION == 'v603':
+        drnum = 'dr12'
+        rnum = 'r5'
+    elif SDSS_VERSION == 'l30e.2':
+        drnum = 'dr13'
+        rnum = 'r6'
+    else:
+        raise RuntimeError('You don\'t appear to be using DR12 or DR13, cannot proceed')
+    fitsfile = 'apVisit-' + rnum + '-' + locID + '-' + mjd + '-' + fiberID + '.fits'
+    fitsfilepath = os.path.join(SDSS_PATH, drnum, 'apogee', 'spectro', 'redux', rnum,
+                                'apo25m', locID, mjd, fitsfile)
+    return fitsfilepath, specfileout, wave, flux, fluxerr
+
+
+def normalize_spec(wave, flux, fluxerr, plot=True):
+    '''
+    runs on a single visit spectrum
+    returns a continuum-normalized spectrum
+    '''
+    contspec = continuum.fitApvisit(flux, fluxerr, wave)
+    specnorm = flux/contspec
+    specnormerr = fluxerr/contspec
+    if plot:
+        plt.plot(wave, flux)
+        plt.plot(wave, contspec, lw=2, color='r')
+        plt.show()
+        plt.plot(wave, specnorm)
+        plt.show()
+    return specnorm, specnormerr
+
+
+def make_BFinfile(fitsfilepath):
+    '''
+    runs on a single visit spectrum
+    returns metadata for use in making a BF_python infile
+    '''
     header = fits.open(fitsfilepath)[0].header
-    HJDlist.append(float('24'+str(header['HJD'])))
-    BCVlist.append(header['BC'])
+    HJD = float('24'+str(header['HJD']))
+    BCV = header['BC']
+    return HJD, BCV
 
-### This part normalizes the spectrum ###
-    contspec = continuum.fitApvisit(fluxes, fluxerrs, waves) #define continuum
-    specnorm = fluxes/contspec #normalization is the spectra divided by the continuum
 
-    # Plot visit spectrum with continuum fit to make sure it looks OK
-    plt.plot(waves, fluxes)
-    plt.plot(waves, contspec, lw=2, color='r')
-    plt.show()
-    plt.plot(waves, specnorm)
-    plt.show()
-
-### Save visit spectrum to text file ###
-    realstar = open(specfileout, 'w')
-    for wave, flux in zip(waves, specnorm): 
-        print(wave, flux, file=realstar)
-    realstar.close()
-    print('Visit spectrum saved to {0}'.format(specfileout))
-
-for file, HJD, BCV in zip(infilelist, HJDlist, BCVlist):
-    print(file, HJD, BCV)
-
+if __name__ == '__main__':
+    main()
