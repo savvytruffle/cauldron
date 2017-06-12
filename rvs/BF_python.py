5fd109313e3414b253e1eba955d74ee2c021241f
Meredith Rawls <mrawls@uw.edu>
2017-06-12 16:26:55 -0700

    Infile rejiggering that may or may not be what you want

--- rvs/BF_python.py
+++ rvs/BF_python.py
@@ -102,9 +102,9 @@ both in days, and the constant RV and BCV of whatever template you are using.
 #outfile =   'data/5284133/5284133Outfile.txt'; bfoutfile = 'data/5284133/5284133BFOut.txt'
 
 #6778289 (despiked (DS) , some visits removed)
-#infiles =   'data/6778289/6778289infilesDS.txt'; bjdinfile = 'data/6778289/6778289bjdinfileDS.txt'
-#gausspars = 'data/6778289/6778289gausspars.txt'
-#outfile =   'data/6778289/6778289OutfileJC.txt'; bfoutfile = 'data/6778289/6778289BFOutJC.txt'
+infiles =   'data/6778289/6778289infilesDS.txt'; bjdinfile = 'data/6778289/6778289bjdinfileDS.txt'
+gausspars = 'data/6778289/6778289gausspars.txt'
+outfile =   'data/6778289/6778289OutfileJC.txt'; bfoutfile = 'data/6778289/6778289BFOutJC.txt'
 
 #6778289 (despiked (DS) , all visits included (AV))
 #infiles =   'data/6778289/6778289infiles.txt'; bjdinfile = 'data/6778289/6778289bjdinfiles.txt'
@@ -117,18 +117,18 @@ both in days, and the constant RV and BCV of whatever template you are using.
 #outfile =   'data/6781535/6781535Outfile.txt'; bfoutfile = 'data/6781535/6781535BFOut.txt'
 
 #6864859
-infiles =   'data/6864859/6864859infilesALL1.txt'; bjdinfile = 'data/6864859/6864859bjdinfileALL1.txt'
-gausspars = 'data/6864859/6864859gaussparsALL1.txt'
-outfile =   'data/6864859/6864859OutfileJCALL1.txt'; bfoutfile = 'data/6864859/6864859BFOutALL1.txt'
+#infiles =   'data/6864859/6864859infilesALL1.txt'; bjdinfile = 'data/6864859/6864859bjdinfileALL1.txt'
+#gausspars = 'data/6864859/6864859gaussparsALL1.txt'
+#outfile =   'data/6864859/6864859OutfileJCALL1.txt'; bfoutfile = 'data/6864859/6864859BFOutALL1.txt'
 
 # ORBITAL PERIOD AND ZEROPOINT !!!
 #period = 2.47028; BJD0 = 2455813.69734 # 4851217
 #period = 3.8994011; BJD0 = 2454959.576010 # 5285607
 #period = 5.7767904; BJD0 = 2456760.90580 # 6449358
 #period = 8.7845759; BJD0 = 245800.46231 #5284133
-#period = 30.13015; BJD0 = 2456557.73097 #6778289
+period = 30.13015; BJD0 = 2456557.73097 #6778289
 #period = 9.1220856; BJD0 = 2456557.733 #6781535
-period = 40.8778427; BJD0 = 2454955.556300 #6864859
+#period = 40.8778427; BJD0 = 2454955.556300 #6864859
 #period = 61.4228063; BJD0 = 2455813.69734 #4075064
 #period = 1.0472603; BJD0 = 2455811.61005 #3848919
 #period = 11.3009948; BJD0 = 2456557.73097 #6610219
@@ -163,8 +163,8 @@ w00 = 15170; n = 10000; stepV = 2.0 # all of APOGEE, still pretty high res
 #widlimits = [0,15, 0,15]; rvneg = -100; rvpos = 100; ymin = -0.15; ymax = 1.19 # good starting default
 #widlimits = [0,16, 0,16]; rvneg = -70; rvpos = 270; ymin = -0.15; ymax = 1.19 # 5285607
 #widlimits = [0,5, 0,5]; rvneg = 100; rvpos = 300; ymin = -0.15; ymax = 1.1 #6449358
-#widlimits = [0,15, 0,8]; rvneg = -50; rvpos = 199; ymin = -0.15; ymax = 1.1 #6778289
-widlimits = [0,12, 0,10]; rvneg = 30; rvpos = 170; ymin = -0.15; ymax = 1.19 # 6864859
+widlimits = [0,15, 0,8]; rvneg = -45; rvpos = 199; ymin = -0.15; ymax = 1.1 #6778289
+#widlimits = [0,12, 0,10]; rvneg = 30; rvpos = 170; ymin = -0.15; ymax = 1.19 # 6864859
 #widlimits = [0,9, 0,9]; rvneg = -150; rvpos = 50; ymin = -0.15; ymax = 1.19 # 6610259a
 #widlimits = [0,15, 0,15]; rvneg = -50; rvpos = 10; ymin = -0.15; ymax = 1.19 # 6610219b
 
