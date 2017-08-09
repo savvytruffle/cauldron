477ac2ac128728f36bdad1128b02a1bc1ee17e7a
Joni Marie Clark Cunningham <jonimarieclarkcunningham@gmail.com>
2017-08-08 20:28:55 -0500

    A bunch of housekeeping.

--- rvs/BF_python.py
+++ rvs/BF_python.py
@@ -97,9 +97,9 @@ both in days, and the constant RV and BCV of whatever template you are using.
 #outfile = 'data/4285087/4285087outfile.txt'; bfoutfile = 'data/4285087/4285087BFOut.txt'
 
 #6131659
-infiles = 'data/6131659/6131659infilesALL.txt'; bjdinfile = 'data/6131659/6131659bjdinfileALL.txt'
-gausspars = 'data/6131659/6131659gaussparsALL.txt'
-outfile = 'data/6131659/6131659outfileALL.txt'; bfoutfile = 'data/6131659/6131659BFOutALL.txt'
+#infiles = 'data/6131659/6131659infiles.txt'; bjdinfile = 'data/6131659/6131659bjdinfile.txt'
+#gausspars = 'data/6131659/6131659gausspars.txt'
+#outfile = 'data/6131659/6131659outfile.txt'; bfoutfile = 'data/6131659/6131659BFOut.txt'
 
 #6449358
 #infiles =   'data/6449358/6449358infiles.txt'; bjdinfile = 'data/6449358/6449358bjdinfile.txt'
@@ -117,9 +117,9 @@ outfile = 'data/6131659/6131659outfileALL.txt'; bfoutfile = 'data/6131659/613165
 #outfile =   'data/6778289/6778289Outfile.txt'; bfoutfile = 'data/6778289/6778289BFOut.txt'
 
 #6781535 (Suspected Triple System)
-#infiles =   'data/6781535/6781535infiles.txt'; bjdinfile = 'data/6781535/6781535bjdinfile.txt'
-#gausspars = 'data/6781535/6781535gausspars.txt'
-#outfile =   'data/6781535/6781535Outfile.txt'; bfoutfile = 'data/6781535/6781535BFOut.txt'
+infiles =   'data/6781535/6781535infiles.txt'; bjdinfile = 'data/6781535/6781535bjdinfile.txt'
+gausspars = 'data/6781535/6781535gausspars.txt'
+outfile =   'data/6781535/6781535Outfile.txt'; bfoutfile = 'data/6781535/6781535BFOut.txt'
 
 #6864859
 #infiles =   'data/6864859/6864859infilesA.txt'; bjdinfile = 'data/6864859/6864859bjdinfileA.txt'
@@ -132,13 +132,13 @@ outfile = 'data/6131659/6131659outfileALL.txt'; bfoutfile = 'data/6131659/613165
 #period = 5.7767904; BJD0 = 2454955.073410 # 6449358
 #period = 8.7845759; BJD0 = 245800.46231 #5284133
 #period = 30.13015; BJD0 = 2456557.73097 #6778289
-#period = 9.1220856; BJD0 = 2454971.834534 #6781535
+period = 9.1220856; BJD0 = 2454971.834534 #6781535
 #period = 40.8778427; BJD0 = 2454955.556300 #6864859
 #period = 61.4228063; BJD0 = 2455813.69734 #4075064
 #period = 1.0472603; BJD0 = 2455811.61005 #3848919
 #period = 11.3009948; BJD0 = 2456557.73097 #6610219
 #period = 4.4860312; BJD0 = 2454966.450124 #4285087 
-period = 17.5278303; BJD0 = 2454960.041397  #6131659
+#period = 17.5278303; BJD0 = 2454960.041397  #6131659
 
 
 
@@ -169,8 +169,8 @@ w00 = 15170; n = 10000; stepV = 2.0 # all of APOGEE, still pretty high res
 
 # CUSTOMIZED BF WIDTH (for gausspars) AND PLOT LIMITS
 #widlimits = [0,15, 0,15]; rvneg = -100; rvpos = 300; ymin = -0.15; ymax = 1.19 # good starting default
-#widlimits = [0,9, 0,10]; rvneg = -75; rvpos = 175; ymin = -0.15; ymax = 1.19 # 6781535
-widlimits = [0,9, 0,9, 0,11]; rvneg = 0; rvpos = 200; ymin = -0.15; ymax = 1.19 # 6131659 
+widlimits = [0,9, 0,10, 0,9]; rvneg = -75; rvpos = 175; ymin = -0.15; ymax = 1.19 # 6781535
+#widlimits = [0,9, 0,9, 0,11]; rvneg = 0; rvpos = 200; ymin = -0.15; ymax = 1.19 # 6131659 
 #widlimits = [0,9, 0,7]; rvneg = -300; rvpos = 300; ymin = -0.15; ymax = 1.19 # 6131659 Xtra large
 #widlimits = [0,13, 0,13]; rvneg = -50; rvpos = 249; ymin = -0.15; ymax = 1.19 # 4285607
 #widlimits = [0,18, 0,19]; rvneg = -70; rvpos = 270; ymin = -0.15; ymax = 1.19 # 5285607
@@ -403,5 +403,5 @@ for i in range (1, nspec):
                       frameon=False, handlelength=3, prop={'size':18})
                       
 plt.show()
-fig.savefig('6136159bfrv.png')
+fig.savefig('6781659bfrv.png')
 #fig.savefig('6781659bfrv.eps')
