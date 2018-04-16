2f0408d1bdab5f86f6827bc8e8931d4ac5f6da1a
Meredith Rawls <mrawls@uw.edu>
2018-04-16 16:23:14 -0700

    Result after going through HR.py code with Joni line by line
    Also delete the pesky pdf plot that kept being regenerated :)

--- rvs/HR.py
+++ rvs/HR.py
@@ -5,6 +5,19 @@ teff for one star and a surface brightness ratio (both on the command line).
 
 It prints out a calculated temperature ratio and teff for the secondary star.
 It also plots the isochrone read in with logg vs. logTeff axes.
+
+Outstanding questions:
+- what to do once we have temperature ratio and teff2
+- what motivates us to choose one isochrone over another (why 1Gyr? why z=0?)
+- wasn't a goal to better constrain radii? How does that get us this?
+    --> maybe because we can now read logg from the y-axis and plus in our known mass and get R?
+        but is that really better?
+- look at Matson paper Fig 8 and what they did to choose isochrones and draw conclusions about
+  evolutionary histories
+- how confident are we that all our systems really are main sequence stars?
+- Goal: be able to plug in KEBLAT results from Matson paper into this (+ other analysis?) and see if 
+  we get similar temperatures, radii, etc. How does Keivan envision this happening?
+
 '''
 
 import numpy as np
@@ -12,107 +25,60 @@ from scipy.interpolate import interp1d
 from sys import argv
 import matplotlib.pyplot as plt
 
-###   Read in the Dartmouth isochrone file   ###
+# Read in the Dartmouth isochrone file
+# For now, default to age of 1 Gyr and metallicity of zero
+# TODO: generalize to more ages and metallicities by reading in different files
 isofile = 'afep0.txt'
-mass, logteff, logg, kp = np.loadtxt(isofile, unpack=True, usecols = (1, 2, 3, 13))
-teff1 = float(argv[1])
-sbratio = float(argv[2])
+isochroneMass, isochroneLogTeff, isochroneLogg, kp = np.loadtxt(isofile, unpack=True, usecols = (1, 2, 3, 13))
+starId = argv[1]
+aspcapTeff = float(argv[2])
+keblatSbRatio = float(argv[3])
 
-#mone = float(argv[3])  # not used
-#mtwo = float(argv[4])  # not used
+# Read in mass, logteff, magnitude from isochrone file (only for points with logg >= 4.1)
+m = np.where(isochroneLogg >= 4.1)  # this restricts the isochrone points used to those with logg >= 4.1
+                           # in effect, this cuts the isochrone down to only include the main sequence.
+isochroneMass = isochroneMass[m]
+isochroneLogTeff = isochroneLogTeff[m]
+isochroneLogg = isochroneLogg[m]
+kp = kp[m]  # magnitude in kepler bandpass
 
-#rone = float(argv[5])  # not used
-#rtwo = float(argv[6])  # not used
+# Calculate two new columns for the isochrone: radius squared and surface brightness
+isochroneRadiusSquared = isochroneMass/(10**isochroneLogg)
+isochroneSb = 10**(-kp/2.5)/isochroneRadiusSquared  # isochrone surface brightness column
 
-########################### Read things from the isochrone file ##############################
-m = np.where(logg >= 4.1)  # a print(m) statement will print all of the masses where the 
-                           # log(g) is greater than 4.1. This isolates members of the main
-                           # sequence. 
-mass = mass[m]
-logteff = logteff[m]
-logg = logg[m]
-kp = kp[m]                                            #magnitude
-r2 = mass/(10**logg)
-sb = 10**(-kp/2.5)/r2                                 #r^2 came from Gm/r^2 = g
+# We want a more fine-grained set of logteff and surface brightness values, so we interpolate
+sb1 = interp1d(isochroneLogTeff, isochroneSb)  # this returns a scipy.interp1d object
+sb1 = sb1(np.log10(aspcapTeff))
+# This estimates sb1, the surface brightness of the primary, which is assumed to correspond to the ASPCAP Teff
+# Because we've interpolated, we can now throw any logTeff we want at it and get a surface brightness back!
 
-# print some stuff out for funsies #
-#print (r2)
+# Now we want to find the location at which Keblat's SB ratio matches the isochroneSb / sb1.
+fit = np.argmin(np.abs(isochroneSb/sb1 - keblatSbRatio))
 
-###   Math Party!   ###
-sb1 = interp1d(logteff, sb)# this interpolates the relationship between the surface bright-
-                           # ness and the effective temperature (mass-luminosity relation)
-                           # which only applies to main sequence stars, which is one of the 
-                           # reasons we need line 21
+# Use the 'fit' index to select the Teff from the isochrone and calculate a temperature ratio
+tratio = 10**isochroneLogTeff[fit]/aspcapTeff
 
+# Now that we have the ratio, we can calculate the temperature of the secondary
+teff2 = aspcapTeff * tratio
 
-
-sb1 = sb1(np.log10(teff1)) # this calculates the surface brightness of the primary, which is 
-                           # assumed to be the surface brightness of the object, because the 
-                           # secondary does not contribute much to the flux observed
-
-###   Fit Party!   ###
-fit = np.argmin(np.abs(sb/sb1 - sbratio))  # this fits a line to the points at which the 
-                                           # the surface brightness calculated for our target
-                           # and the surface brightness ratio that was read in are closest
-                           # to one another. This is then plotted, as it is the evolutionary
-                           # track associated with our particular target. 
-
-
-#fit = fit[0]
-
-###   Ratio Party!   ###
-tratio = 10**logteff[fit]/teff1            # this calculates the temperature ratio using 10
-                                           # to the power of (log(Teff[fit]) using the fit 
-                           # discussed above. This ratio comes from our calculation of the 
-                           # surface brightness, which comes from our calculation of radius 
-                           # of the secondary. This ratio is therefore (Teff of the secondary/
-                           # Teff of the primary.)
-
-teff2 = teff1 * tratio     # this line then calculates the effective temperature of the 
-                           # secondary using the primary Teff and the ratio calculated above. 
-
-loggone = 4.391            # these are for my troubleshooting, attempting to plot log(g) but 
-                           # our calculated points do not fit on the HR diagram, but when we 
-                           # declare loggone as done here, with the log(g)from the Dartmouth 
-                           # file the point is on the track, therefore there is a problem 
-                           # with how radius is calculated or how surface gravity is calculated. 
-
-loggtwo = 4.391
-print('You entered teff 1 as', teff1)
+print('Running analysis for star', starId)
+print('You entered teff 1 as', aspcapTeff)
 print('Using the isochrone file', isofile, 'for star 1,')
 print('We calculate the temperature ratio T2/T1 is', tratio)
 print('Which means teff 2 is', teff2)
 print('Here\'s a plot of the isochrone')
 
-###   Plotting Party!   ###
-#logt1 = np.log10(teff1)
-#logt2 = np.log10(teff2)
-#plt.plot(logt1, rone, 'r.', label='',) 
-#plt.plot(logt2, rtwo, 'b.', label='',)
-#plt.plot(logteff, r, label='',)
-#plt.grid()
-#plt.ylabel('R/$R_{\odot}$')
-#plt.xlabel('log Teff')
-#plt.title('5285607')
-#plt.axis('tight')
-#plt.show()
-#plt.savefig('5285607HR.pdf')
-
+# Plotting stuff
 #log(g) vs. log(teff) complete with Dartmouth evolutionary track
-logt1 = np.log10(teff1)
+logt1 = np.log10(aspcapTeff)
 logt2 = np.log10(teff2)
-#plt.plot(logt1, loggone, color='C1', marker='o', label='Primary') 
-#plt.plot(logt2, loggtwo, color='C2', marker='o', label='Secondary')
-plt.plot(logteff, logg, label='Isochrone used for Primary')
-plt.axvline(np.log10(teff1), color='C1', label='Primary')
+plt.plot(isochroneLogTeff, isochroneLogg, label='Isochrone used for Primary')
+plt.axvline(np.log10(aspcapTeff), color='C1', label='Primary')
 plt.axvline(np.log10(teff2), color='C2', label='Secondary')
 #plt.grid()
 plt.ylabel('$\log g$')
 plt.xlabel('$\log T_{\mathrm{eff}}$')
-plt.title('5285607')
+plt.title(starId)
 #plt.axis('tight')
 plt.legend()
 plt.show()
-plt.savefig('5285607HR.pdf')
-
-
