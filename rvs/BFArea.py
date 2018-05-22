f9ab1b67948127dc8fa37838f35a488fb9fa64de
Joni Marie Cunningham <jonic@nmsu.edu>
2018-05-22 11:39:50 -0700

    Moved BF area calculation from BF_python.py to BFArea.py

--- rvs/BFArea.py
+++ rvs/BFArea.py
@@ -3,7 +3,38 @@ from __future__ import print_function
 import numpy as np
 from scipy.integrate import simps
 from numpy import trapz
+from sys import argv
 
+#Calculate the Area underneath the BF curves 
+
+#PAmp, Perr, PWidth, Samp, Serr, SWidth = np.loadtxt('data/6864859/6864859Gin.txt',
+#	usecols=(0,1,2,3,4,5),unpack=True)
+
+
+#PArea = (PAmp*PWidth)/(2.35*0.3984)
+#PAAve = np.mean(PArea)
+#SArea = (Samp*SWidth)/(2.35*0.3984)
+#SAAve = np.mean(SArea)
+
+#AreaRat = (PArea/SArea)
+#AvAreaRat = (PAAve/SAAve)
+
+#print(AvAreaRat)
+
+#dataout = np.vstack((PArea,SArea))
+#np.savetxt('6864859BFAreaout.txt', dataout.T, fmt = '%.5f')
+
+#BFAreaout = open(areaout, 'w')
+#print(PArea, file=BFAreaout)
+    #print('Secondary BF Area: {0} +/- {1} width {2} xmax {3}'.format(bffitlist[idx][0][3], bffitlist[idx][2][3], bffitlist[idx][0][5], bffitlist[idx][0][4]), file=gout)
+
+#BFAreaout.close()
+    
+#print('BF Areas printed to outfile, Area Ratio star2/star1?=', AvAreaRat)
+
+starId = argv[1] 
+KTRatio = float(argv[2])
+KRsum = float(argv[3])
 
 ### Read in all the things! ###
 
@@ -40,7 +71,15 @@ np.savetxt('6778289BFAreaout.txt', dataout.T, fmt = '%.5f')
 #BFAreaout.close()
     
 #print(SAmed, PAmed)
-print('SArea = ', SArea)
-print('PArea = ', PArea)
+#print('SArea = ', SArea)
+#print('PArea = ', PArea)
 
-#'S/P =', SoP, 'Std =', StD, 'Median =' ,MedAreaRat, 'Mean =', AvAreaRat)
+print('S/P =', SoP, 'Std =', StD, 'Median =' ,MedAreaRat, 'Mean =', AvAreaRat)
+
+RRat = 1/( np.sqrt(SoP/KTRatio ** 4) )
+
+print('R_1/R_2 = ', RRat)
+
+R2 = KRsum / (1 + SoP)
+
+print('R2 = ', R2)
