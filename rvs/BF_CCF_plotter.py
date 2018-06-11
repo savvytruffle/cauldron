10b55e9ce3e49533e12a5ad4047afadaf9746e8f
Meredith Rawls <mrawls@uw.edu>
2018-06-11 16:12:26 -0700

    Do things to make the BF vs CCF plotter better
    This was during the Gaia Sprint
    Includes apStar files for our targets from DR14

--- rvs/BF_CCF_plotter.py
+++ rvs/BF_CCF_plotter.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
@@ -8,43 +9,40 @@ This program makes awesome plots of APOGEE CCFs compared to our BFs.
 The idea is for you to run it once for each target.
 '''
 
-# Where the apStar file lives
-#dir = 'data/5285607/'
-#dir = 'data/6864859/' # promising
-#dir = 'data/6778289/'
-#dir = 'data/6449358/'
-#dir = 'data/4285087/'
-#dir = 'data/6781535/'
-dir = 'data/6131659/' # promising
+KIC = '5285607'
+#KIC = '6864859'
+#KIC = '6131659'
+
+dir = os.path.join('data', KIC)
 
 # The name of the apStar file for a single target
-#ccffile = 'apStar-r5-2M19390532+4027346.fits' #5285607
-#ccffile = 'apStar-r6-2M19292405+4223363.fits' #6864859
-#ccffile = 'apStar-r5-2M19282456+4215080.fits' #6778289
-#ccffile = 'apStar-r5-2M19353513+4149543.fits' #6449358
-#ccffile = 'apStar-r5-2M19463571+3919069.fits' #4285087
-#ccffile = 'apStar-r5-2M19321788+4216489.fits' #6781535
-ccffile = 'apStar-r6-2M19370697+4126128.fits' #6131659
+ccffile = 'apStar-r8-2M19390532+4027346.fits' #5285607
+#ccffile = 'apStar-r8-2M19292405+4223363.fits' #6864859
+#ccffile = 'apStar-r8-2M19282456+4215080.fits' #6778289
+#ccffile = 'apStar-r8-2M19353513+4149543.fits' #6449358
+#ccffile = 'apStar-r8-2M19463571+3919069.fits' #4285087
+#ccffile = 'apStar-r8-2M19321788+4216489.fits' #6781535
+#ccffile = 'apStar-r8-2M19370697+4126128.fits' #6131659
 
 # The name of the file with relevant BF info for the same target
-#bffile = '5285607BFOutAPstar.txt' # in the same order as APOGEE even though it's not chronologic
+bffile = '5285607BFOutAPstar.txt' # in the same order as APOGEE even though it's not chronologic
 #bffile = '6864859BFOutALL.txt'
 #bffile = '6778289BFOut.txt'
 #bffile = '6449358BFOut.txt'
 #bffile = '4285087BFOut.txt'
 #bffile = '6781535BFOut.txt'
-bffile = '6131659BFOutALL.txt'
+#bffile = '6131659BFOutALL.txt'
 
 # The name of the bjdinfile used with BF_python
 # (heliocentric/barycentric velocities in col=(2,); note the top row is for the template)
-#BCVfile = '5285607bjdinfile.txt'
+BCVfile = '5285607bjdinfile.txt'
 #BCVfile = '6864859bjdinfileALL.txt'
-BCVfile = '6131659bjdinfileALL.txt'
+#BCVfile = '6131659bjdinfile.txt'
 
-ccfinfile = dir + ccffile
-bfinfile = dir + bffile
-bjdinfile = dir + BCVfile
-
+#ccfinfile = os.path.join(dir, ccffile)
+ccfinfile = os.path.join('data', ccffile)
+bfinfile = os.path.join(dir, bffile)
+bjdinfile = os.path.join(dir, BCVfile)
 
 # Read in relevant CCF info from apStar file
 hdu = fits.open(ccfinfile)
@@ -53,26 +51,17 @@ hdu9 = hdu[9].data
 CCFvalues = hdu9['CCF'][0][2:]
 CCFerrors = hdu9['CCFERR'][0][2:]
 CCFxaxis = hdu9['CCFLAG'][0]  # pixels, needs to be turned into RVs
-CCF_w0 = hdu9['CCFW0'][0]  # starting wavelength in Angstroms ??
 CCF_delta = hdu9['CCFDW'][0]  # Delta (log_10 lambda)
-# log lambda spacing per lag step (6.d-6 corresponds to 4.145 km/s)
+print(hdu9['VHELIO'][0] - hdu9['VREL'][0])
+# (the data model website says this log lambda spacing per lag step of
+# 6.d-6 corresponds to 4.145 km/s)
 
-# Get the systemic RV for each visit according to APOGEE
-## THIS IS NON-TRIVIAL. APOGEE really likes to remove all RV information.
-## There are VREL1 - VRELNVISIT in HDU0, but they're just BC + VREL = VHELIO.
-## This is not helpful.
-## TODO: figure out actual systemic RVs according to APOGEE
-## (or maybe just move our BFs so they're symmetric around 0 and move on with our lives?)
-#systemics = hdu9['VREL'][0]
-systemics = hdu9['SYNTHVREL'][0]
-#bcvs = hdu9['BC'][0]
-#print(systemics) # these differ from what we think the systemic RV is
-# this would work if the values in systemics were accurate, but they're not
-CCF_rvaxis = []
-for rv in systemics:
-    CCF_rvaxis.append(CCFxaxis * 4.145 + rv)
-# for now, let's just use:
-CCF_rvaxis = CCFxaxis * 4.145
+# Get the systemic RV of the star according to APOGEE
+apSystemic = 82  ## TODO: FIND ACTUAL SYSTEMIC RV PARAMETER !!!
+# Get the barycentric velocities of each visit according to APOGEE
+# This is called VHELIO for super obvious reasons, Jen Sobeck private communication, for reals
+apBCVs = hdu9['VHELIO'][0]
+CCF_rvaxis = [CCFxaxis * 4.145 + bcv for bcv in apBCVs]
 
 # Get the timestamp for each CCF visit from the 0th HDU
 hdu0 = hdu[0].header
@@ -80,7 +69,6 @@ ccftimes = []
 for idx in range(1, len(CCFvalues)+1):
     headercard = 'HJD' + str(idx)
     ccftimes.append(hdu0[headercard] + 2400000.)
-#print(ccftimes)
 
 ccftimesAstropy = []
 for ccftime in ccftimes:
@@ -90,31 +78,17 @@ for ccftime in ccftimes:
 fig = plt.figure(1, figsize=(15,10))
 windowrows = 4
 windowcols = 7
-fig.text(0.5, 0.04, 'Radial Velocity (km s$^{-1}$)', ha='center', va='center', size='small')
-fig.text(0.07, 0.5, 'Arbitrary CCF or BF amplitude', ha='center', va='center', size='small', rotation='vertical')
+fig.text(0.5, 0.04, 'Radial Velocity (km s$^{-1}$)', ha='center', va='center', size='x-large')
+fig.text(0.07, 0.5, 'Arbitrary CCF or BF amplitude', ha='center', va='center', size='x-large', rotation='vertical')
 axlims = [-140, 140, -0.06, 0.56]
 ccfyoffset = -0.12
-#bfxoffset = -100 # arbitrary systemic RV offset to make CCF and BF line up
-#bfxoffset = -61.7 #5285607 visit1
-#bfxoffset = -97.4 #6864859 visit1
-bfxoffset = -100 #613 estimate
-#manualxoffsets = [0, 0, 0, 8, -9, 0, 0,  # 686
-#                  0, 0, 0, 0,  0, 0, 0,
-#                  0, 0, 0, 0,  0, 0, 0,
-#                  0, 0, 0, 0,  0, 0, 0]
-manualxoffsets = [14, 0, 12,  13, 12,  0, -2,  # 613
-                  0,  0, -7,   0,  0,  0, 15,
-                  17, 17, 17, 17, 17, -4, -4,
-                  0,  0,   5, 10, 11, 17]
-#bfxoffset = 0
-#bfyamp = 7 # arbitrary factor to stretch BF values to make CCF and BF line up 686
-bfyamp = 9 #613
+bfyamp = 7 # arbitrary factor to stretch BF values for clarity
 
 # Loop over and plot CCF data
 for idx, CCFdata in enumerate(CCFvalues):
     ax = fig.add_subplot(windowrows, windowcols, idx+1)
     plt.axis(axlims)
-    plt.plot(CCF_rvaxis, CCFvalues[idx] + ccfyoffset)
+    plt.plot(CCF_rvaxis[idx], CCFvalues[idx] + ccfyoffset)
     plt.text(23, 0.5, ccftimesAstropy[idx].iso[0:10], size=10)
 #    plt.text(25, 0.5, '{0:.3f}'.format(ccftimes[idx] - 2450000.), size=10, color='C0')
 #    plt.text(25, 0.3, idx, size=10, color='C0')
@@ -141,7 +115,6 @@ for idx, (rv, value, error) in enumerate(zip(BFrvaxis, BFvalues, BFerrors)):
 # Read in barycentric velocity correction info from the BJD infile
 BCVdata = np.loadtxt(bjdinfile, comments='#', usecols=(2,), unpack=True)
 BCVdata = BCVdata[1:]
-#print(BCVdata)
 
 # Loop over and plot BF data
 for idx in range(0, len(bftimes)):
@@ -159,11 +132,10 @@ for idx in range(0, len(bftimes)):
 #    plt.text(25, 0.4, '{0:.3f}'.format(bftimes[idx] - 2450000.), size=10, color='C1')
 #    plt.text(25, 0.2, idx, size=10, color='C1')
     try:
-        plt.plot(BFrvaxis[BFindices[idx]:BFindices[idx+1]] + bfxoffset + BCVdata[idx] + manualxoffsets[idx], 
+        plt.plot(BFrvaxis[BFindices[idx]:BFindices[idx+1]] - apSystemic + BCVdata[idx], 
                  bfyamp*BFvalues[BFindices[idx]:BFindices[idx+1]])
     except: # handle the final case where there is no idx+1
-        plt.plot(BFrvaxis[BFindices[idx]::] + bfxoffset + BCVdata[idx] + manualxoffsets[idx], 
+        plt.plot(BFrvaxis[BFindices[idx]::] - apSystemic + BCVdata[idx], 
                  bfyamp*BFvalues[BFindices[idx]::])
 
 plt.show()
-#fig.savefig('6136159BFCCF.png')
