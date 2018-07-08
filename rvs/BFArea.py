1bcce6aa299767d9e56f25090b9b38350c801ecc
Joni Marie Cunningham <jonic@nmsu.edu>
2018-07-07 19:17:08 -0700

    More propagation of errors in BFArea.py

--- rvs/BFArea.py
+++ rvs/BFArea.py
@@ -18,7 +18,9 @@ from sys import argv
 areaout = 'data/5285607/5285607BFArea.txt'
 starId = 5285607
 KTRatio = 0.4922
+KTRaterr = 0.1086
 KRsum = 3.489
+KRsumerr = 0.051
 LCM1 = 1.554
 LCM1err = 0.023
 LCM2 = 1.333
@@ -29,7 +31,9 @@ LCM2err = 0.020
 areaout = 'data/6864859/6864859BFArea.txt'
 starId = 6864859
 KTRatio = 0.4965
-KRsum = 3.104
+KTRaterr = 0.1143
+KRsum = 1.042
+KRsumerr = 0.016
 LCM1 = 1.354
 LCM1err = 0.029
 LCM2 = 1.411
@@ -39,7 +43,9 @@ LCM2err = 0.028
 areaout = 'data/6778289/6778289BFArea.txt'
 starId = 6778289
 KTRatio = 0.4865
-KRsum =2.745
+KTRaterr = 0.2673
+KRsum =0.7226
+KRsumerr = 0.0086
 LCM1 = 1.510
 LCM1err = 0.022
 LCM2 = 1.091
@@ -49,7 +55,9 @@ LCM2err = 0.018
 areaout = 'data/4285607/4285607BFArea.txt'
 starId = 4285607
 KTRatio = 0.969
-KRsum =2.033
+KTRaterr = 0.1405
+KRsum =0.9696
+KRsumerr = 0.0080
 LCM1 = 1.137
 LCM1err = 0.013
 LCM2 = 1.103
@@ -59,7 +67,9 @@ LCM2err = 0.014
 areaout = 'data/6131659/6131659BFArea.txt'
 starId = 6131659
 KTRatio = 0.6599
-KRsum =1.5251
+KTRaterr = 0.2192
+KRsum =0.7458
+KRsumerr = 0.0052
 LCM1 = 0.9422
 LCM1err = 0.0093
 LCM2 = 0.7028
@@ -69,7 +79,9 @@ LCM2err = 0.0078
 areaout = 'data/6781535/6781535BFArea.txt'
 starId = 6781535
 KTRatio = 0.5562
-KRsum = 2.0408
+KTRaterr = 0.3508
+KRsum = 1.0283
+KRsumerr = 0.0213
 LCM1 = 1.0057
 LCM1err = 0.0327
 LCM2 = 1.0346
@@ -114,12 +126,23 @@ print('S/P =', SoP, 'Std =', StD, 'Median =' ,MedAreaRat, 'Mean =', AvAreaRat)
 
 RRat = 1/( np.sqrt(SoP/KTRatio ** 4) )
 
-print('R_1/R_2 = ', RRat)
+### This is where I attempt to calculate the error in the radius ratio, reasoning as follows:
+  # For powers and square roots, you multiply the relative standard error by the power, in 
+  # our ratio calculation this meant multiplying by powers of (-1), (1/2), and (4). The (-1) 
+  # power comes from the 1 over the function, which is equivalent to the function to the (-1)
+  # power. The (1/2) power comes from the square root. Finally the power of (4) comes from the 
+  # fourth power in the denominator of the Rrat calculation, which I am not sure I have treated 
+  # correctly in this case. 
+  
+RRatstd = -2 * np.absolute( SoP / KTRatio ) * np.sqrt ( (StD / SoP ) ** 2 + ( KTRaterr / KTRatio ) ** 2 - 2 *  ( ( StD * KTRaterr ) / ( SoP * KTRatio ) ))
+
+print('R_1/R_2 = ', RRat, '+/- = ', RRatstd)
 
 R2 = KRsum / (1 + SoP)
+R2err = np.abs (R2) * ( np.sqrt ( ( KRsumerr / KRsum ) ** 2 + ( StD / SoP ) ** 2 ) )
 
 R1 = KRsum - R2
-
+R1err = np.sqrt( KRsumerr ** 2 + R2err ** 2 + 2 * KRsumerr * R2err )
 print('R_1 = ', R1, 'R2 = ', R2)
 
 ###Now, calculate log(g) to put on our HR diagrams from KEBLAT masses and individual radii just found###
