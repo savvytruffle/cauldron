ce0cda8fdce5f2deaeb65cf6fe201b88e4340a2c
Meredith Rawls <mrawls@uw.edu>
2019-04-01 17:00:39 -0700

    Update BF_CCF plotter and move some files around

--- rvs/BF_CCF_plotter.py
+++ rvs/BF_CCF_plotter.py
@@ -9,33 +9,36 @@ This program makes awesome plots of APOGEE CCFs compared to our BFs.
 The idea is for you to run it once for each target.
 '''
 
+#plt.rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+#plt.rc('text', usetex=True)
+
 #KIC = '5285607'; windowrows = 1; windowcols = 6
 #KIC = '6864859'; windowrows = 4; windowcols = 7
 #KIC = '6778289'; windowrows = 4; windowcols = 7
 #KIC = '6449358'; windowrows = 4; windowcols = 7
-KIC = '4285087'; windowrows = 1; windowcols = 6
+#KIC = '4285087'; windowrows = 1; windowcols = 6
 #KIC = '6781535'; windowrows = 4; windowcols = 7
-#KIC = '6131659'; windowrows = 4; windowcols = 7
+KIC = '6131659'; windowrows = 4; windowcols = 7
 
 dir = os.path.join('data', KIC)
 
 # The name of the apStar file for a single target
 #ccffile = 'apStar-r8-2M19390532+4027346.fits' #5285607
-#ccffile = 'apStar-r8-2M19292405+4223363.fits' #6864859
+#ccffile = 'apStar-r6-2M19292405+4223363.fits' #6864859  #r8 is missing one visit
 #ccffile = 'apStar-r8-2M19282456+4215080.fits' #6778289
 #ccffile = 'apStar-r8-2M19353513+4149543.fits' #6449358
-ccffile = 'apStar-r8-2M19463571+3919069.fits' #4285087
+#ccffile = 'apStar-r8-2M19463571+3919069.fits' #4285087
 #ccffile = 'apStar-r8-2M19321788+4216489.fits' #6781535
-#ccffile = 'apStar-r8-2M19370697+4126128.fits' #6131659
+ccffile = 'apStar-r8-2M19370697+4126128.fits' #6131659
 
 # The name of the file with relevant BF info for the same target
 #bffile = '5285607BFOutAPstar.txt' # in the same order as APOGEE even though it's not chronologic
 #bffile = '6864859BFOutALL.txt'
 #bffile = '6778289BFOutJCAllVisits.txt'
 #bffile = '6449358BFOutALL.txt'
-bffile = '4285087BFOut.txt'
+#bffile = '4285087BFOut.txt'
 #bffile = '6781535BFOutALL.txt'
-#bffile = '6131659BFOutALL.txt'
+bffile = '6131659BFOutALL.txt'
 
 # The name of the bjdinfile used with BF_python
 # (heliocentric/barycentric velocities in col=(2,); note the top row is for the template)
@@ -43,9 +46,9 @@ bffile = '4285087BFOut.txt'
 #BCVfile = '6864859bjdinfileALL.txt'
 #BCVfile = '6778289bjdinfiles.txt'
 #BCVfile = '6449358bjdinfile.txt'
-BCVfile = '4285087bjdinfile.txt'  ## apogee visits aren't chronological WHY
+#BCVfile = '4285087bjdinfile.txt'  ## apogee visits aren't chronological WHY
 #BCVfile = '6781535bjdinfileALL.txt'
-#BCVfile = '6131659bjdinfile.txt'
+BCVfile = '6131659bjdinfile.txt'
 
 #ccfinfile = os.path.join(dir, ccffile)
 ccfinfile = os.path.join('data', ccffile)
@@ -94,7 +97,7 @@ ax.spines['top'].set_color('none')
 ax.spines['bottom'].set_color('none')
 ax.spines['left'].set_color('none')
 ax.spines['right'].set_color('none')
-plt.title(KIC, size='x-large')
+plt.title('KIC ' + KIC, size='x-large')
 
 #windowrows = 4
 #windowcols = 7
@@ -102,17 +105,18 @@ plt.title(KIC, size='x-large')
 #fig.text(0.07, 0.5, 'Arbitrary CCF or BF amplitude', ha='center', va='center', size='x-large', rotation='vertical')
 axlims = [-140, 140, -0.06, 0.56]
 ccfyoffset = -0.12
-bfyamp = 7 # arbitrary factor to stretch BF values for clarity
+bfyamp = 9 # arbitrary factor to stretch BF values for clarity
+# used 7 for 686 and 9 for 613
 
 # Loop over and plot CCF data
 for idx, CCFdata in enumerate(CCFvalues):
-    ax = fig.add_subplot(windowrows, windowcols, idx+1)
+    ax0 = fig.add_subplot(windowrows, windowcols, idx+1)
     plt.axis(axlims)
     plt.plot(CCF_rvaxis[idx], CCFvalues[idx] + ccfyoffset)
-    #plt.text(23, 0.5, ccftimesAstropy[idx].iso[0:10], size=10)
+    plt.text(23, 0.5, ccftimesAstropy[idx].iso[0:10], size=10)
     ## TURN ON FOR TIMESTAMP TROUBLESHOOTING
-    plt.text(25, 0.5, '{0:.3f}'.format(ccftimes[idx] - 2450000.), size=10, color='C0')
-    plt.text(25, 0.3, idx, size=10, color='C0')
+    #plt.text(25, 0.5, '{0:.3f}'.format(ccftimes[idx] - 2450000.), size=10, color='C0')
+    #plt.text(25, 0.3, idx, size=10, color='C0')
     plt.subplots_adjust(wspace=0, hspace=0)   
 
 # Read in relevant BF info from the BF infile
@@ -139,20 +143,20 @@ BCVdata = BCVdata[1:]
 
 # Loop over and plot BF data
 for idx in range(0, len(bftimes)):
-    ax = fig.add_subplot(windowrows, windowcols, idx+1)
+    ax1 = fig.add_subplot(windowrows, windowcols, idx+1)
     plt.axis(axlims)
-    ax.yaxis.set_major_locator(ticker.MultipleLocator(0.2))
-    for tick in ax.xaxis.get_major_ticks():
+    ax1.yaxis.set_major_locator(ticker.MultipleLocator(0.2))
+    for tick in ax1.xaxis.get_major_ticks():
         tick.label.set_fontsize(12)
-    for tick in ax.yaxis.get_major_ticks():
+    for tick in ax1.yaxis.get_major_ticks():
         tick.label.set_fontsize(12)
-    if (idx!= 18 and idx!=19 and idx!=20 and idx!=21 and idx!=22 and idx!=23 and idx!=24 and idx!=25 and idx!=26):
-        ax.set_xticklabels(())
+    if (idx < 20):  # set to 18 for 686 and 20 for 613
+        ax1.set_xticklabels(())
     if (idx!=0 and idx!=7 and idx!=14 and idx!=21):
-        ax.set_yticklabels(())
+        ax1.set_yticklabels(())
     ## TURN ON FOR TIMESTAMP TROUBLESHOOTING
-    plt.text(25, 0.4, '{0:.3f}'.format(bftimes[idx] - 2450000.), size=10, color='C1')
-    plt.text(25, 0.2, idx, size=10, color='C1')
+    #plt.text(25, 0.4, '{0:.3f}'.format(bftimes[idx] - 2450000.), size=10, color='C1')
+    #plt.text(25, 0.2, idx, size=10, color='C1')
     try:
         plt.plot(BFrvaxis[BFindices[idx]:BFindices[idx+1]] - apSystemic + BCVdata[idx], 
                  bfyamp*BFvalues[BFindices[idx]:BFindices[idx+1]])
